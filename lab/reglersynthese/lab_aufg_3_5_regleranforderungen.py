c45087b3a5688da8dd7d4b93f0a32bba1b2fee97
Philipp Rapp <philipprapp@gmx.de>
2024-04-19 07:40:50 +0200

    Added reference implementations for lab 03

---
+++ lab/reglersynthese/lab_aufg_3_5_regleranforderungen.py
@@ -0,0 +1,78 @@
+# %% Pakete
+
+import numpy as np
+import matplotlib.pyplot as plt
+import control as ctrl
+
+# %% Teil (a)
+
+omega_n = 3.0
+zeta = 0.1
+
+G = ctrl.tf([omega_n**2], [1, 2*zeta*omega_n, omega_n**2])
+
+t, y = ctrl.step_response(G)
+
+plt.figure(1)
+plt.clf()
+plt.plot(t, y)
+plt.grid()
+plt.xlim(0,1.0)
+plt.show()
+
+# %% Teil (b)
+
+omega_n = 1.0
+zeta = 0.49
+
+G = ctrl.tf([omega_n**2], [1, 2*zeta*omega_n, omega_n**2])
+
+t, y = ctrl.step_response(G)
+
+plt.figure(1)
+plt.clf()
+plt.plot(t, y)
+plt.grid()
+# plt.xlim(0,1.0)
+plt.show()
+
+# %% Teil (c)
+
+rho = 0.5
+zeta = 0.05
+omega_n = rho / zeta
+
+G = ctrl.tf([omega_n**2], [1, 2*zeta*omega_n, omega_n**2])
+
+t, y = ctrl.step_response(G)
+
+plt.figure(1)
+plt.clf()
+plt.plot(t, y)
+plt.grid()
+# plt.xlim(0,1.0)
+plt.show()
+
+# %% Teil (c)
+
+
+omega_n = 5.0
+zeta = 0.7
+rho = zeta*omega_n
+print(f'rho = {rho}')
+assert rho > 0.5
+
+G = ctrl.tf([omega_n**2], [1, 2*zeta*omega_n, omega_n**2])
+
+t, y = ctrl.step_response(G)
+
+plt.figure(1)
+plt.clf()
+plt.plot(t, y)
+plt.grid()
+# plt.xlim(0,1.0)
+plt.show()
+
+
+
+# %%
---
+++ lab/reglersynthese/lab_aufg_3_8_nullstellen.py
@@ -0,0 +1,33 @@
+# %% Pakete
+
+import numpy as np
+import matplotlib.pyplot as plt
+import control as ctrl
+
+# %%
+
+omega_n = 1.0
+zeta = 0.5
+alpha = -10
+
+G_no_zero = ctrl.tf([1], [1/omega_n**2, 2*zeta/omega_n, 1])
+
+G = ctrl.tf([1/(alpha*zeta*omega_n), 1], [1/omega_n**2, 2*zeta/omega_n, 1])
+print(G)
+
+# %%
+
+t_, y_ = ctrl.step_response(G_no_zero)
+t, y = ctrl.step_response(G)
+
+plt.figure('Step response')
+plt.plot(t,y)
+plt.plot(t_, y_, linestyle='dashed')
+plt.grid()
+plt.show()
+
+plt.figure('Pol-Nullstellen')
+ctrl.pzmap(G)
+
+
+# %%
